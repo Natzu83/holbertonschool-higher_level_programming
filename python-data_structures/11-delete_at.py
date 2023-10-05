1207c04996696e1f7d3c5ca73e35286011a1b17a
Natzu83 <natzuac@yahoo.com>
2023-10-05 16:33:20 -0400

    Delete yourself

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    # Check if idx is negative or out of range
     if idx < 0 or idx >= len(my_list):
-        return my_list  # Return the same list if idx is invalid
+        return (my_list)
 
-    # Create a new list without the element at the specified index
-    new_list = [my_list[i] for i in range(len(my_list)) if i != idx]
-    return new_list
+    del my_list[idx]
+    return (my_list)
+
+
+
+
+
+

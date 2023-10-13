640635e38aa89ff81aca4a2f7aa784a7774ef0d7
Natzu83 <natzuac@yahoo.com>
2023-10-13 16:50:19 -0400

    Define

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is a Class Square that defines a square by (based on 0-square.py) """
+
+
+class Square:
+    """Represent a square"""
+    def __init__(self, size):
+        """Square Atributes"""
+        self.__size = size

4ce57ac6b911f35f06cbdc105bc0b245d54b10f9
Natzu83 <natzuac@yahoo.com>
2023-10-20 07:59:55 -0400

    Explanation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+""" Create a sorted copy of the list"""
 class MyList(list):
     def print_sorted(self):
-        sorted_list = sorted(self)  # Create a sorted copy of the list
+        sorted_list = sorted(self)
         print(sorted_list)
 
-# Example usage:
+""" Example usage: """
 my_list = MyList([5, 1, 3, 2, 4])
 my_list.print_sorted()

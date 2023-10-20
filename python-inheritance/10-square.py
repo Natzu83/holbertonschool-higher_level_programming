5c11cbad83142ba0b92a7f51e29c727ab33bb322
Natzu83 <natzuac@yahoo.com>
2023-10-20 19:59:08 -0400

    Test

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,43 +1,33 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    def area(self):
-        raise Exception("area() is not implemented")
+"""
+Rectangle that inherits from BaseGeometry
 
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+Instantiation with width and height: def __init__(self, width, height)::
+width and height must be private. No getter or setter
+width and height must be positive integers validated by integer_validator
+the area() method must be implemented
+print() should print, and str() should return, the following rectangle
+    description: [Rectangle] <width>/<height>
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
+    """
+    This is a Rectangle class!
+    """
     def __init__(self, width, height):
+        """This is the Initialization Method"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        return self.__width * self.__height
+        """Returns area of rectangle"""
+        return (self.__width * self.__height)
 
     def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
-    def __repr__(self):
-        return self.__str()
-
-class Square(Rectangle):
-    def __init__(self, size):
-        self.integer_validator("size", size)
-        self.__width = size
-        self.__height = size
-
-    def __str__(self):
-        return f"[Square] {self.__width}/{self.__height}"
-
-    def __repr__(self):
-        return self.__str()
-
-# Example usage:
-square = Square(5)
-print(square.area())  # Prints the area: 25
-print(str(square))    # Prints the square description: [Square] 5/5
+        """Return string representation of rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

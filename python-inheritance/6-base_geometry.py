2df42ca99b9e2659599cee68f2e542279b956bea
Natzu83 <natzuac@yahoo.com>
2023-10-20 19:33:13 -0400

    Hot fix

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Public instance method: def area(self): that raises an
+    Exception with the message area() is not implemented
+"""
+
+
 class BaseGeometry:
+    """Only raises exception"""
     def area(self):
+        """area method"""
         raise Exception("area() is not implemented")

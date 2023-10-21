3110d62dfacf0c3a5c5e28b9c39330700ce3c823
Natzu83 <natzuac@yahoo.com>
2023-10-20 20:17:38 -0400

    Second try

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""A function that reads a text file (UTF8) and prints it to stdout"""
+"""Defines a text file reading function."""
+
+
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as file:
+    """Module function that reads a text file (UTF8)"""
+    with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             print(line, end="")

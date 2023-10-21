f3bec048ca453410d5646abd8192b11b999033ab
Natzu83 <natzuac@yahoo.com>
2023-10-21 15:08:48 -0400

    New way plz work

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
-Write a script that adds all arguments to a
-    Python list, and then save them to a file:
+   Add the item 
 """
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+filename = "add_item.json"
+current_list = []
 
-try:
-        existing_data = load_from_json_file('add_item.json')
-except:
-    existing_data = []
-
-new_items = sys.argv[1:]
-updated_data = existing_data + new_items
-
-# for i in range(1, len(sys.argv)):
-#     existing_data.append(sys.argv[i])
-
-save_to_json_file(updated_data, 'add_item.json')
-# save_to_json_file(existing_data, 'add_item.json')
+if len(sys.argv) == 1:
+        save_to_json_file(current_list, filename)
+else:
+    try:
+        current_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        pass
+    finally:
+        args = sys.argv[1:]
+        for i in args:
+            current_list.append(i)
+        save_to_json_file(current_list, filename)

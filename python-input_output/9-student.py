14098a1a6123d7c1168bee1cf0fb61fc140e63a3
Natzu83 <natzuac@yahoo.com>
2023-10-21 20:41:16 -0400

    Student JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """Class that defines a student by
+    first_name, last_name, age)"""
+
+    def __init__(self, first_name, last_name, age):
+        """Defining attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Dictionary representation of a Student instance"""
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+            }

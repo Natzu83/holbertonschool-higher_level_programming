9c32c2507f2340197aae152728a145ba5c2fe901
Natzu83 <natzuac@yahoo.com>
2023-10-26 17:40:01 -0400

    Typo

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -33,12 +33,12 @@ class Square(Rectangle):
         self.__height = value
 
     def update(self, *args, **kwargs):
-        """Updates attributes of an instance"""
+        """Updates attributes of the instance"""
 
         if args is not None and len(args) != 0:
             if len(args) >= 1:
                 if type(args[0]) != int and args[0] is not None:
-                    raise TypeError("id must be an integer")
+                    raise TypeError("id is an integer")
                 self.id = args[0]
             if len(args) > 1:
                 self.size = args[1]
@@ -50,7 +50,7 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 if key == "id":
                     if type(value) != int and value is not None:
-                        raise TypeError("id must be an integer")
+                        raise TypeError("id is an integer")
                     self.id = value
                 if key == "size":
                     self.size = value

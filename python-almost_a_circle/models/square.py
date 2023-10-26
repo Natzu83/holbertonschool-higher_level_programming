54b03e9b413b3f4466bb6bd8ee90bdcf8c3203ce
Natzu83 <natzuac@yahoo.com>
2023-10-26 10:57:21 -0400

    The base of the square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Contains a square class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = None
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Defines the string representation of the class"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"

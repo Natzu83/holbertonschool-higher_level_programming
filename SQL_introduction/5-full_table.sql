8352ff089642ea1463891fab46e2443882009247
Natzu83 <natzuac@yahoo.com>
2023-11-21 19:44:42 -0400

    First try

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,1 +1,3 @@
-Placeholder
+-- A script that prints the full description of the table first_table.
+-- It's not allowed to use the DESCRIBE or EXPLAIN statements because Holberton lol
+SHOW CREATE TABLE first_table;

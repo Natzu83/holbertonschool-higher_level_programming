fd062dc51a03bd46a910dfe666f9f6208a35c173
Natzu83 <natzuac@yahoo.com>
2023-11-21 19:41:48 -0400

    Lets go

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,5 @@
 -- A script that creates a table called from the first_table in the current database
-CREATE TABLE IF NOT EXISTS first_table
+CREATE TABLE IF NOT EXISTS first_table (
+    id int,
+    name varchar(256)
+);

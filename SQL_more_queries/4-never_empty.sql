d80a89e377c86bec5bed337db95a796f1e9ba8b4
Natzu83 <natzuac@yahoo.com>
2023-11-21 22:52:59 -0400

    Add the id

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,6 @@
 -- A script that creates the table id_not_null
 -- If the table force_name already exists, the script should not fail
 CREATE TABLE IF NOT EXISTS id_not_null (
+    id int DEFAULT 1 NOT NULL,
+    name varchar(256)
+);

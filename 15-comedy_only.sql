7aa1b7ec6144c5a64b012fb77b7961dc0357c992
Natzu83 <natzuac@yahoo.com>
2023-11-21 23:57:37 -0400

    Hot fix

--- 15-comedy_only.sql
+++ 15-comedy_only.sql
@@ -1,4 +1,8 @@
--- A script  that lists all Comedy shows
+-- Script  that lists all Comedy shows
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- Use only one SELECT statement
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id

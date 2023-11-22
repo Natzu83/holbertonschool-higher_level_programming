7f9c1fcaae1ada18cfdd1afd799366cb86d1ff1d
Natzu83 <natzuac@yahoo.com>
2023-11-21 20:10:05 -0400

    Create the table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,1 +1,15 @@
-Placeholder
+-- Create a table second_table in the database to hbtn_0c_0
+-- If the table second_table already exists, then the script should not fail
+-- It's not allowed to use the SELECT and SHOW statements
+CREATE TABLE IF NOT EXISTS second_table (
+    id int,
+    name varchar(256),
+    score int
+);
+-- Insert 4 new records
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);

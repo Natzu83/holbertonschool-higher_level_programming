1fb8e033dc6340ced733078199c3d949af127ea1
Natzu83 <natzuac@yahoo.com>
2023-11-21 22:49:15 -0400

    Force the name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,1 +1,6 @@
-Placeholder
+-- A script that creates the table force_name
+-- If the table force_name already exists, then the script should not fail
+CREATE TABLE IF NOT EXISTS force_name (
+    id int,
+    name varchar(256) NOT NULL
+);

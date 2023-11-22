52b4cd189ca456e90fcc9aeaac0fe0655552519e
Natzu83 <natzuac@yahoo.com>
2023-11-21 21:05:38 -0400

    Comments

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,1 +1,5 @@
-Placeholder
+-- A script that lists all records of the table second_table
+-- Don’t list the rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score 
+SELECT score, name

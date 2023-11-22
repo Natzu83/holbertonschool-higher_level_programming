50e0831d36999c772035b98fda366e29c87100f3
Natzu83 <natzuac@yahoo.com>
2023-11-21 23:21:53 -0400

    Unique

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,1 +1,6 @@
-Placeholder
+-- A script that creates the table unique_id
+-- If the table unique_id already exists, the script should not fail
+CREATE TABLE IF NOT EXISTS unique_id (
+    id int DEFAULT 1 NOT NULL UNIQUE,
+    name varchar(256)
+);

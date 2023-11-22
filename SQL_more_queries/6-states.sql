24420e7e64f2fe2625f5def16f9f5060f1bcecc6
Natzu83 <natzuac@yahoo.com>
2023-11-21 23:23:36 -0400

    The states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,1 +1,9 @@
-Placeholder
+-- A script that creates the database hbtn_0d_usa and the table states
+-- If the table states already exists, the script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id int AUTO_INCREMENT NOT NULL UNIQUE,
+    name varchar(256) NOT NULL,
+    PRIMARY KEY (id)
+);

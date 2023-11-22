ed12e132539f5679c3b2c407c70efe147c71753c
Natzu83 <natzuac@yahoo.com>
2023-11-21 20:55:29 -0400

    The score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,1 +1,6 @@
-Placeholder
+-- A script  that lists the number of records with the same score 
+-- The result should display:
+-- Score
+-- Number of records for the score with the label number
+-- List should be sorted by the number of records (descending)
+SELECT score, COUNT(score) AS number

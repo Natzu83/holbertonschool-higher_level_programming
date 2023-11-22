bf39e3a5b1a76a759378dd687e9f734c969be5f7
Natzu83 <natzuac@yahoo.com>
2023-11-21 20:27:27 -0400

    Best score

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,1 +1,7 @@
-Placeholder
+-- A script that lists all records with a score >= 10 
+-- The results should display both the score and the name (in this order)
+-- The records should be ordered by score (top first)
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;

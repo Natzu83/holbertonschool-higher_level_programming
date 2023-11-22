ea053d9b5c984e2889116576ba5f507bd225cc28
Natzu83 <natzuac@yahoo.com>
2023-11-21 23:38:04 -0400

    All shows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,1 +1,5 @@
-Placeholder
+-- A script  that lists all shows contained on the hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

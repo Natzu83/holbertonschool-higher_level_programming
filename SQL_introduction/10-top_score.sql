e189b93354c5914143288aede41199d38bb1c6d2
Natzu83 <natzuac@yahoo.com>
2023-11-21 20:18:00 -0400

    First try

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,1 +1,6 @@
-Placeholder
+-- A script that lists all records of the table in the second_table
+-- Results displayed both in the score and the name (in this order)
+-- Records should be ordered by score (top first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;

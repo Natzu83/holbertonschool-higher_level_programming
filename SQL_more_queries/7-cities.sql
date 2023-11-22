2073e75733e0f019a3fc9d860af7c2840e1864d2
Natzu83 <natzuac@yahoo.com>
2023-11-21 23:25:57 -0400

    The cites

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,1 +1,12 @@
-Placeholder
+-- A script that creates the database hbtn_0d_usa and the table of cities
+-- If the database hbtn_0d_usa already exists, the script should not fail
+-- If the table cities already exists, the script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT NOT NULL UNIQUE,
+    state_id INT NOT NULL,
+    name varchar(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);

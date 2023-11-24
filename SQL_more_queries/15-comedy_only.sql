d3d8935787b5a80b8af5645129391a5b192c7abc
Natzu83 <natzuac@yahoo.com>
2023-11-24 10:15:45 -0400

    Move to correct dir y typo fix

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
--- script that lists all Comedy shows in the database hbtn_0d_tvshows.
+-- A script that lists all Comedy shows in the database hbtn_0d_tvshows.
 
 SELECT tv_shows.title
 FROM tv_shows

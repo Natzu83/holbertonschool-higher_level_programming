bf1f3aa8ce1b8526babd1d3bdea2ee30ef3f83da
Natzu83 <natzuac@yahoo.com>
2023-11-26 23:08:42 -0400

    First try

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,21 @@
-Placeholder
+#!/usr/bin/python3
+'''The changes of the name of a state object
+from the database hbtn_0e_6_usa'''
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}\
+                           @localhost:3306/{argv[3]}', pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id == 2).first()
+    if state is not None:
+        state.name = 'New Mexico'
+        session.commit()
+    else:
+        print('State not found')

59cdb927a2b4c1877432c83cefb9c127995ae341
Natzu83 <natzuac@yahoo.com>
2023-11-26 21:06:43 -0400

    Hot fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,3 +9,8 @@ Base = declarative_base()
 
 
 class State(Base):
+    '''Class that inhertis from Base'''
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)

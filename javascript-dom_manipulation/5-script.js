fdb2cb5bc847dfa1b2be0a06c218336b6f760cb6
Natzu83 <natzuac@yahoo.com>
2024-02-02 20:29:55 -0400

    Script 5

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,1 +1,8 @@
-Placeholder
+document.addEventListener('DOMContentLoaded', (event) => {
+  let updateHeader = document.querySelector('#update_header');
+  let header = document.querySelector('header');
+
+  updateHeader.addEventListener('click', () => {
+      header.textContent = 'New Header!!!';
+    });
+ });

67990c0e0b06d9fbd12fa6ea99b8f35f4829c335
Natzu83 <natzuac@yahoo.com>
2024-02-02 20:13:50 -0400

    Script 2

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,1 +1,8 @@
-Placeholder
+document.addEventListener('DOMContentLoaded', (event) => {
+  let redHeader = document.querySelector('#red_header');
+  let header = document.querySelector('header');
+
+  redHeader.addEventListener('click', () => {
+    header.classList.add('red');
+  });
+});

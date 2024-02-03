d9adef19ef592f0f34b1d2bb0cb3c6277bdce893
Natzu83 <natzuac@yahoo.com>
2024-02-02 20:47:36 -0400

    Script 8

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,1 +1,9 @@
-Placeholder
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+      .then(response => response.json())
+      .then(data => {
+        const div = document.getElementById('hello');
+        div.textContent = data.hello;
+      })
+      .catch(error => console.error('Error:', error));
+  });

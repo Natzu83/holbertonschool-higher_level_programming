5c79554512b9a27925fdff59ea6cd237ebdab4a8
Natzu83 <natzuac@yahoo.com>
2024-02-02 20:34:23 -0400

    Script 6

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,1 +1,9 @@
-Placeholder
+document.addEventListener('DOMContentLoaded', (event) => {
+  fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+      .then(response => response.json())
+      .then(data => {
+           let character = document.querySelector('#character');
+           character.textContent = data.name;
+       })
+        .catch(error => console.error(error));
+   });
